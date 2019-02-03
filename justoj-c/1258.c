19a704de79dc365ff1e5c75481243d5cdd79f559
ismdeep <ismdeep@live.com>
2019-02-03 16:55:20 +0800

    feat: add solution(C) for JustOJ1258

---
+++ justoj-c/1258.c
@@ -0,0 +1,13 @@
+//
+// Created by ismdeep on 2019-02-03.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main() {
+    printf("x=2.000\n");
+    return 0;
+}
