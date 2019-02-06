a58f1a28c4cd680ba4aa3c10c89514e09a182287
ismdeep <ismdeep@live.com>
2019-02-06 21:12:49 +0800

    feat: add solution(Python3) for JustOJ1244 feat: add solution(Python3) for JustOJ1260

---
+++ justoj-py3/1244.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# author: ismdeep
+# dateime: 2019-02-06 21:02:01
+# filename: 1244.py
+# blog: https://ismdeep.com
+s = list(input().strip())
+s.sort()
+print(*s)
---
+++ justoj-py3/1260.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# author: ismdeep
+# dateime: 2019-02-06 21:08:17
+# filename: 1260.py
+# blog: https://ismdeep.com
+print(8 + 2 * int(input()))
