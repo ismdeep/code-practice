c080e82b19719d538aaa45ff742ef396a58b4e3a
ismdeep <ismdeep@live.com>
2019-02-22 14:15:39 +0800

    feat: add solution(PYTHON3) for JustOJ1875

---
+++ justoj-py3/1875.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# author: ismdeep
+# dateime: 2019-02-22 14:09:40
+# filename: 1875.py
+# blog: https://ismdeep.com
+
+ans = 1
+for i in range(1, 1977 + 1):
+    ans *= i
+print(ans)
+
