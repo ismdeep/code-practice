5a89eb9b4c4b8d6d1b110f048a56afd1b1cc86a4
ismdeep <ismdeep@live.com>
2019-02-23 22:23:06 +0800

    feat: add github-today-contributions.py

---
+++ github-today-contributions/github-today-contributions.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+# author: ismdeep
+# dateime: 2019-02-22 17:17:39
+# filename: github-today-contributions.py
+# blog: https://ismdeep.com
+
+import requests
+import time
+import sys
+import re
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+def findall(content, pattern_str):
+    pattern = re.compile(pattern_str, re.S)
+    return re.findall(pattern, content)
+
+
+def main():
+    date_str = time.strftime('%Y-%m-%d', time.localtime())
+    user = ''
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == '-user':
+            user = sys.argv[i + 1]
+    url = 'https://github.com/users/%s/contributions?to=%s' % (user, date_str)
+    req = requests.get(
+        url=url
+    )
+    days = findall(req.text,
+                   '''<rect class\="day" width\="(.*?)" height\="(.*?)" x\="(.*?)" y\="(.*?)" fill\="#(.*?)" data-count\="(.*?)" data-date\="(.*?)"/>''')
+    for day in days[-31::]:
+        print(day[6], day[5])
+
+
+if __name__ == '__main__':
+    main()
