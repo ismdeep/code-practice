4a6de460d0bb888aa6a39a3c19a28e8f45711362
ismdeep <ismdeep@live.com>
2019-02-26 23:05:15 +0800

    feat: add dynamic-arry.cpp for demos

---
+++ c-lang-practices/test/dynamic-array.cpp
@@ -0,0 +1,16 @@
+//
+// Created by ismdeep on 2019-02-26.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    size_t n;
+    cin >> n;
+    uint64_t a[n];
+    for (size_t i = 0; i < n; i++) {
+        a[i] = i;
+    }
+    return 0;
+}
