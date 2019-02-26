ee51ff2d24e5b41953379ef02b457fdd7389615f
ismdeep <ismdeep@live.com>
2019-02-26 14:25:54 +0800

    feat: add solution(PYTHON3) for JustOJ2177

---
+++ justoj-py3/2177.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+# author: ismdeep
+# dateime: 2019-02-26 14:22:00
+# filename: 2177.py
+# blog: https://ismdeep.com
+a1, a2, n = map(int, input().split())
+print(a1 + (a2 - a1) * (n - 1))
