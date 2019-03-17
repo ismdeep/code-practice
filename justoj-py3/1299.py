0dc4d10426d537b7b38e678aef96556ccd95e656
ismdeep <ismdeep@live.com>
2019-03-17 22:37:49 +0800

    refact: refact code for justoj-py3/1299.py

--- justoj-py3/1299.py
+++ justoj-py3/1299.py
@@ -3,43 +3,46 @@
 # dateime: 2019-03-04 14:17:43
 # filename: 1299.py
 # blog: https://ismdeep.com
-trans = [
-    ('T', '0', 'T'),
-    ('T', '1', 'T'),
-    ('T', '2', 'T'),
-    ('T', '3', 'T'),
-    ('T', '4', 'T'),
-    ('T', '5', 'T'),
-    ('T', '6', 'T'),
-    ('T', '7', 'T'),
-    ('T', '8', 'T'),
-    ('T', '9', 'T')
-]
-start_state = 'T'
-terminals = ['T']
 
 
-def dfa(_str_):
-    state = start_state
+def dfa(_str_, _start_state_, _trans_, _terminals_):
+    state = _start_state_
     for item in _str_:
         found = False
         new_state = ''
-        for (_from_, _link_, _to_) in trans:
+        for (_from_, _link_, _to_) in _trans_:
             if _from_ == state and _link_ == item:
                 found = True
                 new_state = _to_
         if not found:
             return False
         state = new_state
-    return True if state in terminals else False
+    return True if state in _terminals_ else False
+
+
+def expand_links(_pre_trans_, _expand_map_):
+    _trans_ = []
+    for _from_, _link_, _to_ in _pre_trans_:
+        if _link_ in _expand_map_:
+            for item in _expand_map_[_link_]:
+                _trans_.append((_from_, item, _to_))
+        else:
+            _trans_.append((_from_, _link_, _to_))
+    return _trans_
 
 
 def main():
+    pre_trans = [
+        ('T', 'd', 'T')
+    ]
+    trans = expand_links(pre_trans, {'d': list('0123456789')})
     while True:
         try:
             s = input()
             s = s.strip()
-            print('accept' if dfa(s) else 'not accept')
+            if '' is s:
+                continue
+            print('accept' if dfa(s, 'T', trans, ['T']) else 'not accept')
         except:
             exit(0)
 
