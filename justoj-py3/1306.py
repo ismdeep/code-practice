fcf1b42dd01fe095e40d5eceb879471be5bd718c
ismdeep <ismdeep@live.com>
2019-03-18 16:01:25 +0800

    refact: refact code for justoj-py3/1306.py

--- justoj-py3/1306.py
+++ justoj-py3/1306.py
@@ -1,4 +1,41 @@
-import queue
+from queue import Queue
+
+
+def convert_nfa_to_dfa(_trans_, _start_state_, _terminals_):
+    link_set = []
+    for _from_, _link_, _to_ in _trans_:
+        if _link_ not in link_set:
+            link_set.append(_link_)
+    done = set()
+    done.add(str([_start_state_]))
+    q = Queue()
+    q.put(str([_start_state_]))
+    dfa_trans = []
+    while q.qsize() > 0:
+        states = eval(q.get())
+        for _todo_link_ in link_set:
+            next_states = []
+            for state in states:
+                for _from_, _link_, _to_ in _trans_:
+                    if _from_ == state and _link_ == _todo_link_:
+                        next_states.append(_to_)
+            next_states2 = []
+            for item in next_states:
+                if item not in next_states2:
+                    next_states2.append(item)
+            next_states2.sort()
+            dfa_trans.append((str(states), _todo_link_, str(next_states2)))
+            if str(next_states2) not in done:
+                q.put(str(next_states2))
+                done.add(str(next_states2))
+    return dfa_trans
+
+
+def extract_trans(_trans_):
+    trans = []
+    for tran in _trans_:
+        trans.append((tran[0], tran[1], tran[2]))
+    return trans
 
 
 def state_to_str(arr):
@@ -11,82 +48,54 @@ def state_to_str(arr):
     return ans
 
 
-trans = []
-pre_trans = [
-    '0a0', '0b0', '0a3', '3a4', '4a4', '4b4', '0b1', '1b2', '2a2', '2b2'
-]
-
-dfa_trans = []
-done = set()
-
-start_state = '0'
-terminals = ['2', '4']
-
-q = queue.Queue()
-q.put([start_state])
-
-for pre_tran in pre_trans:
-    trans.append((pre_tran[0],pre_tran[1],pre_tran[2]))
-
-
-while q.qsize() > 0:
-    states = q.get()
-    for _todo_link_ in ['a', 'b']:
-        next_states = []
-        for state in states:
-            for _from_, _link_, _to_ in trans:
-                if _from_ == state and _link_ == _todo_link_:
-                    next_states.append(_to_)
-        next_states2 = []
-        for item in next_states:
-            if item not in next_states2:
-                next_states2.append(item)
-        next_states2.sort()
-        dfa_trans.append((str(states), _todo_link_, str(next_states2)))
-        if str(next_states2) not in done:
-            q.put(next_states2)
-            done.add(str(next_states2))
-
-def dfa(_str_):
-    cur_state = '''['0']'''
+def dfa(_str_, _start_state_, _trans_, _terminals_):
+    cur_state = str([_start_state_])
     routines = [cur_state]
-    for item in _str_:
+    for _item_ in _str_:
         found = False
         new_state = ''
-        for (_from_, _link_, _to_) in dfa_trans:
-            if _from_ == cur_state and _link_ == item:
+        for (_from_, _link_, _to_) in _trans_:
+            if _from_ == cur_state and _link_ == _item_:
                 found = True
                 new_state = _to_
         if not found:
-            return (False, routines)
+            return False, routines
         cur_state = new_state
         routines.append(cur_state)
     cur_state = eval(cur_state)
     terminals_in_state = False
-    for terminal in terminals:
+
+    for terminal in _terminals_:
         if terminal in cur_state:
             terminals_in_state = True
     return (True, routines) if terminals_in_state else (False, routines)
 
 
-s = ''
-while True:
-    try:
-        s = input().strip()
-        status, routines = dfa(s)
-        routine_strs = []
-        for routine in routines:
-            routine_strs.append(state_to_str(eval(routine)))
-        for i in range(len(s) + 1 - len(routine_strs)):
-            routine_strs.append('{}')
-        print(routine_strs[0], end='')
-        for i in range(len(s)):
-            print('-' + s[i] + '->', end='')
-            print(routine_strs[i+1], end='')
-        print()
-        if status:
-            print('accept')
-        else:
-            print('not accept')
-    except:
-        exit(0)
+###########################################################################
+
+
+def main():
+    trans = extract_trans(['0a0', '0b0', '0a3', '3a4', '4a4', '4b4', '0b1', '1b2', '2a2', '2b2'])
+    trans = convert_nfa_to_dfa(trans, '0', ['2', '4'])
+
+    while True:
+        try:
+            s = input().strip()
+            status, routines = dfa(s, '0', trans, ['2', '4'])
+            for i in range(len(s) + 1 - len(routines)):
+                routines.append('[]')
+            print(state_to_str(eval(routines[0])), end='')
+            for i in range(len(s)):
+                print('-' + s[i] + '->', end='')
+                print(state_to_str(eval(routines[i + 1])), end='')
+            print()
+            if status:
+                print('accept')
+            else:
+                print('not accept')
+        except:
+            exit(0)
+
+
+if __name__ == '__main__':
+    main()
