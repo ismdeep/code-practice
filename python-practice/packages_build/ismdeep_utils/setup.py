b25f0eba1c3debc17b962c1482a1963c15c27fa0
ismdeep <ismdeep@live.com>
2019-04-12 09:04:19 +0800

    feat: add ismdeep_utils

---
+++ python-practice/packages_build/ismdeep_utils/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from setuptools import setup
+from ismdeep_utils import __version__ as version
+
+setup(
+    name='ismdeep_utils',
+    version=version,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    description='Some useful utils from ismdeep',
+    author='ismdeep',
+    author_email='ismdeep@protonmail.com',
+    url='https://github.com/ismdeep/ismdeep_utils_py',
+    py_modules=['ismdeep_utils']
+)
