275f070e0b6bdbb6aad0d4a393a1ff636dfae1d5
ismdeep <ismdeep@live.com>
2019-04-13 22:03:36 +0800

    update: bigint.c

--- c-lang-practices/ds/bigint/bigint.c
+++ c-lang-practices/ds/bigint/bigint.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define MOD 10000
 
@@ -19,11 +20,11 @@ int digital_count(int val) {
 }
 
 
-char compare_sign(int val) {
-    if (0 == val) return '=';
-    if (val > 0) return '>';
-    if (val < 0) return '<';
-}
+//char compare_sign(int val) {
+//    if (0 == val) return '=';
+//    if (val > 0) return '>';
+//    return '<';
+//}
 
 
 struct BigInt {
@@ -60,10 +61,12 @@ struct BigInt *create_bigint(int val) {
 struct BigInt *create_bigint_from_str(const char str[]) {
     struct BigInt *bigint;
     bigint = (struct BigInt *) malloc(sizeof(struct BigInt));
-    int len = strlen(str);
+    int len = (int) strlen(str);
 
-    bigint->sign = 0; if ('-' == str[0]) { bigint->sign = 1; }
+    bigint->sign = 0;
+    if ('-' == str[0]) { bigint->sign = 1; }
     bigint->data = (int *) malloc(sizeof(int) * (len / 4 + 2));
+    memset(bigint->data, 0, sizeof(int) * (len / 4 + 2));
     bigint->length = len / 4 + 2;
     bigint->top = -1;
     int stop = bigint->sign == 1 ? 0 : -1;
@@ -71,7 +74,7 @@ struct BigInt *create_bigint_from_str(const char str[]) {
     for (int i = len - 1; i > stop; i -= 4) {
         ++bigint->top;
         bigint->data[bigint->top] = 0;
-        int from = i - 3 > stop + 1 ? i - 4 : stop + 1;
+        int from = i - 3 > stop + 1 ? i - 3 : stop + 1;
         for (int j = from; j <= i; ++j) {
             bigint->data[bigint->top] *= 10;
             bigint->data[bigint->top] += (str[j] - '0');
@@ -87,6 +90,7 @@ struct BigInt *bigint_abs(const struct BigInt *bigint_a) {
     ans->sign = 0;
     ans->length = bigint_a->length;
     ans->data = (int *) malloc(sizeof(int) * bigint_a->length);
+    memset(ans->data, 0, sizeof(int) * bigint_a->length);
     for (int i = 0; i < ans->length; ++i) {
         ans->data[i] = bigint_a->data[i];
     }
@@ -116,9 +120,102 @@ int bigint_compare(const struct BigInt *a, const struct BigInt *b) {
     return 0;
 }
 
-struct BigInt *bigint_add(struct BigInt *a, struct BigInt *b) {
+struct BigInt *bigint_add(const struct BigInt *a, const struct BigInt *b);
+
+struct BigInt *bigint_subtract(const struct BigInt *a, const struct BigInt *b) {
+    if (a == b) return create_bigint(0);
+    if (0 == bigint_compare(a, b)) return create_bigint(0);
+
+    if (0 == a->sign && 1 == b->sign) {
+        struct BigInt *ans = bigint_add(a, bigint_abs(b));
+        return ans;
+    }
+
+    if (1 == a->sign && 0 == b->sign) {
+        struct BigInt *ans = bigint_add(bigint_abs(a), b);
+        return ans;
+    }
+
+    if (1 == a->sign && 1 == b->sign) {
+        struct BigInt *ans = bigint_subtract(bigint_abs(b), bigint_abs(a));
+//        ans->sign = 1 - ans->sign;
+        return ans;
+    }
+
+    if (-1 == bigint_compare(a, b)) {
+        struct BigInt *ans = bigint_subtract(b, a);
+        ans->sign = 1;
+        return ans;
+    }
+
     struct BigInt *ans;
     ans = (struct BigInt *) malloc(sizeof(struct BigInt));
+    ans->length = a->length;
+    ans->data = (int *) malloc(sizeof(int) * ans->length);
+    ans->sign = 0;
+    memset(ans->data, 0, sizeof(int) * ans->length);
+
+    /* copy data ans subtract */
+    for (int i = 0; i <= a->top; ++i) {
+        ans->data[i] = a->data[i];
+    }
+    for (int i = 0; i <= b->top; ++i) {
+        ans->data[i] -= b->data[i];
+    }
+
+    /* arrange data */
+    for (int i = 0; i < ans->length - 1; ++i) {
+        while (ans->data[i] < 0) {
+            --ans->data[i + 1];
+            ans->data[i] += MOD;
+        }
+    }
+
+    ans->top = ans->length - 1;
+    while (ans->top > 0 && ans->data[ans->top] == 0) --ans->top;
+
+    return ans;
+}
+
+struct BigInt *bigint_add(const struct BigInt *a, const struct BigInt *b) {
+    if (1 == a->sign && 1 == b->sign) {
+        struct BigInt *ans = bigint_add(bigint_abs(a), bigint_abs(b));
+        ans->sign = 1;
+        return ans;
+    }
+
+    if (1 == a->sign && 0 == b->sign) {
+        struct BigInt *ans = bigint_subtract(b, bigint_abs(a));
+        return ans;
+    }
+
+    if (0 == a->sign && 1 == b->sign) {
+        struct BigInt *ans = bigint_subtract(a, bigint_abs(b));
+        return ans;
+    }
+
+    struct BigInt *ans;
+    ans = (struct BigInt *) malloc(sizeof(struct BigInt));
+    ans->length = a->top > b->top ? a->top + 3 : b->top + 3;
+    ans->data = (int *) malloc(sizeof(int) * ans->length);
+    memset(ans->data, 0, sizeof(int) * ans->length);
+    ans->top = -1;
+
+    /* copy and block add */
+    for (int i = 0; i <= a->top; ++i) {
+        ans->data[i] = a->data[i];
+    }
+    for (int i = 0; i <= b->top; ++i) {
+        ans->data[i] += b->data[i];
+    }
+
+    /* arrange data */
+    for (int i = 0; i < ans->length - 1; ++i) {
+        ans->data[i + 1] += ans->data[i] / MOD;
+        ans->data[i] %= MOD;
+    }
+    ans->top = ans->length - 1;
+    while (ans->top > 0 && ans->data[ans->top] == 0) --ans->top;
     return ans;
 }
 
@@ -130,51 +227,47 @@ char *bigint_2_string(const struct BigInt *bigint) {
     top -= (4 - digital_count(bigint->data[bigint->top]));
     data[top + 1] = '\0';
     int stop = 0;
-    if (bigint->sign == 1) { stop = 1; ++top; data[0] = '-'; }
+    if (bigint->sign == 1) {
+        stop = 1;
+        ++top;
+        data[0] = '-';
+    }
     for (int i = 0; i <= bigint->top; ++i) {
         int val = bigint->data[i];
         for (int timeid = 0; timeid < 4; ++timeid) {
-            if (top >= stop) { data[top--] = '0' + (val % 10); val /= 10; }
+            if (top >= stop) {
+                data[top--] = (char) ('0' + (val % 10));
+                val /= 10;
+            }
         }
     }
     return data;
 }
 
 
-
-void print_bigint(const struct BigInt *bigint) {
-    printf("%p => %s\n", bigint, bigint_2_string(bigint));
-}
-
-
-void test_compare(const struct BigInt *a, const struct BigInt *b) {
-    printf("%s %c %s\n", bigint_2_string(a), compare_sign(bigint_compare(a, b)), bigint_2_string(b));
-}
-
-
 int main(int argc, char *argv[]) {
     struct BigInt *bigint_a = create_bigint(2309847);
-    printf("bigint_a => %p\noutput: {%s}\nexpect: {2309847}\n\n", bigint_a, bigint_2_string(bigint_a));
+    assert(strcmp("2309847", bigint_2_string(bigint_a)) == 0);
 
     struct BigInt *bigint_b = create_bigint(-2309847);
-    printf("bigint_b => %p\noutput: {%s}\nexpect: {-2309847}\n\n", bigint_b, bigint_2_string(bigint_b));
+    assert(strcmp("-2309847", bigint_2_string(bigint_b)) == 0);
 
     struct BigInt *bigint_c = create_bigint_from_str("23974997920438");
-    printf("bigint_c => %p\noutput: {%s}\nexpect: {23974997920438}\n\n", bigint_c, bigint_2_string(bigint_c));
+    assert(strcmp("23974997920438", bigint_2_string(bigint_c)) == 0);
 
     struct BigInt *bigint_d = create_bigint_from_str("-23974997920438");
-    printf("bigint_d => %p\noutput: {%s}\nexpect: {-23974997920438}\n\n", bigint_d, bigint_2_string(bigint_d));
+    assert(strcmp("-23974997920438", bigint_2_string(bigint_d)) == 0);
 
     struct BigInt *bigint_e = create_bigint_from_str("10000000");
-    printf("bigint_e => %p\noutput: {%s}\nexpect: {10000000}\n\n", bigint_e, bigint_2_string(bigint_e));
+    assert(strcmp("10000000", bigint_2_string(bigint_e)) == 0);
 
-    test_compare(bigint_a, bigint_a);
-    test_compare(bigint_a, bigint_b);
-    test_compare(bigint_a, bigint_c);
-    test_compare(bigint_a, bigint_d);
-    test_compare(bigint_b, bigint_c);
-    test_compare(bigint_b, bigint_d);
-    test_compare(bigint_c, bigint_d);
-    test_compare(bigint_d, bigint_c);
+    assert(bigint_compare(bigint_a, bigint_a) == 0);
+    assert(bigint_compare(bigint_a, bigint_b) == 1);
+    assert(bigint_compare(bigint_a, bigint_c) == -1);
+    assert(bigint_compare(bigint_a, bigint_d) == 1);
+    assert(bigint_compare(bigint_a, bigint_e) == -1);
+
+    printf("%s\n", bigint_2_string(bigint_subtract(bigint_d, bigint_b)));
+
     return 0;
 }
