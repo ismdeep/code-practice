a0cc91c22e50a6ed5e71e1d0a54ded26c7d6c7d7
ismdeep <ismdeep@live.com>
2019-04-16 14:47:04 +0800

    update

--- python-practice/clean-zsh-history-redundant.py
+++ python-practice/clean-zsh-history-redundant.py
@@ -7,20 +7,23 @@
 import os
 import sys
 import codecs
+from ismdeep_utils import DateTimeUtil
 
 ans = []
 data_set = set()
 black_list = ['exit', 'cd ~', 'lc']
 
+os.system('cp /Users/ismdeep/.zsh_history /Users/ismdeep/.zsh_history_%d' % (DateTimeUtil.unix_timestamp_now_second()))
+
 with codecs.open('/Users/ismdeep/.zsh_history', 'r', 'UTF-8', 'ignore') as f:
     for line in f.readlines():
         line = line.strip()
-        print('{%s}' % line)
         left = line[:line.find(':0;')] + ':0;'
         right = line[line.find(':0;') + 3:]
         if right not in data_set and right not in black_list:
             ans.append(line + '\n')
             data_set.add(right)
+            print(right)
 
 with codecs.open('/Users/ismdeep/.zsh_history', 'w', 'utf-8', 'ignore') as f:
     f.writelines(ans)
