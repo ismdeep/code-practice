41e4a107fa9f07a5d03b7a3369e9b2f0e34c862b
ismdeep <ismdeep@live.com>
2019-05-10 21:21:38 +0800

    Refactor send data handles' codes

--- python-practice/http-server/server.py
+++ python-practice/http-server/server.py
@@ -4,6 +4,7 @@ import platform
 from socket import *
 import threading
 from time import gmtime, strftime
+from urllib.parse import unquote
 
 HOST = '0.0.0.0'
 PORT = 80
@@ -85,54 +86,123 @@ def parse_file_path(_config_, _url_):
     sections = _url_.split('/')
     for section in sections:
         if len(section) > 0:
-            _path_ += directory_separator + section
+            _path_ += directory_separator + unquote(section)
     return _path_
 
 
-def generate_data_package(_content_):
-    ans = ('HTTP/1.1 200 OK\r\n'
-           'Date: %(datetime)s\r\n'
-           'Server: Honix\r\n'
-           'X-Powered-By: HonixPY\r\n'
-           'Content-Length: %(datalen)d\r\n\r\n%(content)s') % \
-          ({
-              'datetime': strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime()),
-              'datalen': len(_content_),
-              'content': _content_
-          })
-    return ans
+def parse_file_type(_file_path_):
+    file_type_map = {
+        'ico': 'image/x-icon',
+        'gif': 'image/gif',
+        'jpg': 'image/jpeg',
+        'png': 'image/png',
+        'pdf': 'application/pdf',
+        'html': 'text/html;charset=utf-8'
+    }
+    ext_name = _file_path_[_file_path_.rfind('.')+1:].lower()
+    return file_type_map[ext_name] if ext_name in file_type_map else 'ostream'
 
 
-def generate_file_data_package(_file_path_, _range_from_, _range_to_):
+def send_file_data_handle(_config_, _tcpclient_, _file_path_):
     file_size = os.path.getsize(_file_path_)
-    _range_to_ = min(_range_to_, file_size - 1)
-    f = open(_file_path_, 'rb')
-    f.seek(_range_from_)
-    ans = ''
-    ans += 'HTTP/1.1 200 OK\r\n'
-    ans += 'Date: %s\r\n' % strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
-    ans += 'Server: Apache\r\n'
-    ans += 'Content-Length: %d\r\n' % file_size
-    ans += 'Connection: close\r\n'
-    ans += 'Content-Type: text/html;charset=utf-8\r\n\r\n'
-    return ans.encode(), _range_from_, _range_to_, f
+    range_to = file_size - 1
+    range_from = 0
+    file = open(_file_path_, 'rb')
+    headers = ''
+    headers += 'HTTP/1.1 200 OK\r\n'
+    headers += 'Date: %s\r\n' % strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+    headers += 'Server: %s\r\n' % _config_['server']
+    headers += 'Content-Length: %d\r\n' % file_size
+    headers += 'Connection: close\r\n'
+    headers += 'Content-Type: %s\r\n\r\n' % parse_file_type(_file_path_)
+    _tcpclient_.send(headers.encode())
+    while range_from <= range_to:
+        length = min(range_to - range_from + 1, 1024 * 1024 * 4)
+        data = file.read(length)
+        range_from += length
+        _tcpclient_.send(data)
+    _tcpclient_.close()
 
 
-def generate_404(_config_):
-    file_path = _config_['root'] + directory_separator + _config_['error_page_404']
+def get_files(_file_path_):
+    for root, dirs, files in os.walk(_file_path_):
+        if _file_path_ == root:
+            return dirs, files
+    return [], []
+
+
+def send_file_list_handle(_config_, _tcpclient_, _file_path_):
+    dirs, files = get_files(_file_path_)
+    headers = ''
+    headers += 'HTTP/1.1 200 OK\r\n'
+    headers += 'Date: %s\r\n' % strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+    headers += 'Server: %s\r\n' % _config_['server']
+    headers += 'Connection: close\r\n'
+    headers += 'Content-Type: text/html;charset=utf-8\r\n\r\n'
+    content = open(_config_['tpl'] + directory_separator + 'file-list.html', 'r').read()
+    content_file = open(_config_['tpl'] + directory_separator + 'file-list-file.html', 'r').read()
+    content_dir = open(_config_['tpl'] + directory_separator + 'file-list-dir.html', 'r').read()
+    content_files = ''
+    content_dirs = ''
+    for file in files:
+        tmp = content_file
+        tmp = tmp.replace('{$file_name}', file)
+        content_files += tmp
+    for dir in dirs:
+        tmp = content_dir
+        tmp = tmp.replace('{$dir_name}', dir)
+        content_dirs += tmp
+    content = content.replace('{$files}', content_files)
+    content = content.replace('{$dirs}', content_dirs)
+    _tcpclient_.send(headers.encode())
+    _tcpclient_.send(content.encode())
+    _tcpclient_.close()
+
+
+def send_404_handle(_config_, _tcp_client_):
+    file_path = _config_['tpl'] + directory_separator + '404.html'
     file_size = os.path.getsize(file_path)
     f = open(file_path, 'rb')
-    ans = ''
-    ans += 'HTTP/1.1 404 Not Found\r\n'
-    ans += 'Server: Honix\r\n'
-    ans += 'Date: %s\r\n' % strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
-    # ans += 'Accept-Ranges: bytes\r\n'
-    ans += 'Content-Length: %d\r\n' % file_size
-    # ans += 'Cache-Control: max-age=600\r\n'
-    ans += 'Connection: close\r\n'
-    ans += 'Status: 404\r\n'
-    ans += 'Content-Type: text/html;charset=utf-8\r\n\r\n'
-    return ans.encode(), f
+    headers = ''
+    headers += 'HTTP/1.1 404 Not Found\r\n'
+    headers += 'Server: %s\r\n' % _config_['server']
+    headers += 'Date: %s\r\n' % strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+    headers += 'Content-Length: %d\r\n' % file_size
+    headers += 'Connection: close\r\n'
+    headers += 'Status: 404\r\n'
+    headers += 'Content-Type: text/html;charset=utf-8\r\n\r\n'
+    _tcp_client_.send(headers.encode())
+    _tcp_client_.send(f.read())
+    _tcp_client_.close()
+
+
+def send_502_handle(_config_, _tcp_client_):
+    file_path = _config_['tpl'] + directory_separator + '404.html'
+    file_size = os.path.getsize(file_path)
+    f = open(file_path, 'rb')
+    headers = ''
+    headers += 'HTTP/1.1 502\r\n'
+    headers += 'Server: Honix\r\n'
+    headers += 'Date: %s\r\n' % strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+    headers += 'Content-Length: %d\r\n' % file_size
+    headers += 'Connection: close\r\n'
+    headers += 'Status: 502\r\n'
+    headers += 'Content-Type: text/html;charset=utf-8\r\n\r\n'
+    _tcp_client_.send(headers.encode())
+    _tcp_client_.send(f.read())
+    _tcp_client_.close()
+
+
+def send_redirect_data_handle(_config_, _tcp_client_, _redirect_url_):
+    headers = ''
+    headers += 'HTTP/1.1 301 Moved Permanently\r\n'
+    headers += 'Server: Honix\r\n'
+    headers += 'Date: %s\r\n' % strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime())
+    headers += 'Connection: close\r\n'
+    headers += 'Location: %s\r\n' % _redirect_url_
+    headers += 'Content-Type: text/html;charset=utf-8\r\n\r\n'
+    _tcp_client_.send(headers.encode())
+    _tcp_client_.close()
 
 
 def tcp_handle(_tcpclient_):
@@ -142,35 +212,21 @@ def tcp_handle(_tcpclient_):
     host_name = request_header['host']
     config = server_configs[host_name] if host_name in server_configs else server_configs['default']
     file_path = parse_file_path(config, request_header['url'])
+    print(file_path)
+    if os.path.isfile(file_path):
+        send_file_data_handle(config, _tcpclient_, file_path)
+        return
+    if os.path.isdir(file_path) and os.path.isfile(file_path + directory_separator + config['default_file']):
+        send_file_data_handle(config, _tcpclient_, file_path + directory_separator + config['default_file'])
+        return
+    if os.path.isdir(file_path) and request_header['url'][len(request_header['url'])-1:] != '/':
+        send_redirect_data_handle(config, _tcpclient_, 'http://' + request_header['host'] + request_header['url'] + '/')
+        return
     if os.path.isdir(file_path):
-        if os.path.isfile(file_path + directory_separator + config['default_file']):
-            file_path = file_path + directory_separator + config['default_file']
-            header, range_from, range_to, file = generate_file_data_package(file_path, 0, 239847289374927)
-            _tcpclient_.send(header)
-            while range_from <= range_to:
-                length = min(range_to - range_from + 1, 1024 * 1024 * 4)
-                data = file.read(length)
-                range_from += length
-                _tcpclient_.send(data)
-            _tcpclient_.close()
-        else:
-            '''send back file list'''
-            _tcpclient_.send('test'.encode())
-            _tcpclient_.close()
-    elif os.path.isfile(file_path):
-        header, range_from, range_to, file = generate_file_data_package(file_path, 0, 239847289374927)
-        _tcpclient_.send(header)
-        while range_from <= range_to:
-            length = min(range_to - range_from + 1, 1024 * 1024 * 4)
-            data = file.read(length)
-            range_from += length
-            _tcpclient_.send(data)
-        _tcpclient_.close()
-    else:
-        header, file = generate_404(config)
-        _tcpclient_.send(header)
-        _tcpclient_.send(file.read())
-        _tcpclient_.close()
+        '''send back file list'''
+        send_file_list_handle(config, _tcpclient_, file_path)
+        return
+    send_404_handle(config, _tcpclient_)
 
 
 def main():
