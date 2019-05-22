34d1993298eb443fb85ea1b164cf658cb55b1390
ismdeep <ismdeep@live.com>
2019-05-22 09:12:47 +0800

    Fix thread-pool-demo

--- c-lang-practices/thread-pool-demo/main.cpp
+++ c-lang-practices/thread-pool-demo/main.cpp
@@ -1,62 +1,51 @@
 #include <iostream>
 #include <unistd.h>
 #include <thread>
+#include <mutex>
 
 using namespace std;
 
 #define TIMES(id, n) for (int id = 0; id < (n); ++id)
 #define TIMESRANGE(id, from, to) for (int id = (from); id <= (to); ++id)
 #define TIMESREVERSE(id, fromlarge, tosmall) for (int id = (fromlarge); id >= (tosmall); --id)
-#define THREAD_SIZE 2
 
+#define THREAD_SIZE 4
 
-int pool[THREAD_SIZE];
+int running_count = 0;
+mutex _mutex_;
+
 
 int fib(int n) {
-    return n <= 1 ? 1 : fib(n - 1) + fib(n - 2);
+    return n < 2 ? 1 : fib(n - 1) + fib(n - 2);
 }
 
 
 void thread_fun(int mm) {
-    printf("thread_fun(%d)\n", mm);
-    while (true) {
-        if (-1 != pool[mm]) {
-            printf("%d => %d\n", mm, fib(40));
-            pool[mm] = -1;
-        }
-        sleep(1);
-    }
-
-}
-
-
-void worker() {
-    int mm = 0;
-    while (mm < 256) {
-        TIMES(workderid, THREAD_SIZE) {
-            if (-1 == pool[workderid] && mm < 256) {
-                pool[workderid] = mm;
-                ++mm;
-            }
-        }
-        sleep(1);
-    }
+//    printf("entering thread_fun(%d)\n", mm);
+    printf("%d: fib(%d)\n", mm, fib(42));
+//    printf("exiting thread_fun(%d)\n", mm);
+    _mutex_.lock();
+    --running_count;
+    _mutex_.unlock();
 }
 
 
 int main() {
-    TIMES(i, THREAD_SIZE) {
-        pool[i] = -1;
+    thread t[100];
+    for (int i = 0; i < 100;) {
+        if (running_count < THREAD_SIZE) {
+            t[i] = thread(thread_fun, i);
+            _mutex_.lock();
+            ++running_count;
+            _mutex_.unlock();
+            ++i;
+        } else {
+            usleep(10000);
+        }
     }
 
-    thread t[THREAD_SIZE];
-    TIMES(threadid, THREAD_SIZE) {
-        t[threadid] = thread(thread_fun, threadid);
+    for (int i = 0; i < 100; ++i) {
+        t[i].join();
     }
-
-    TIMES(threadid, THREAD_SIZE) {
-        t[threadid].join();
-    }
-
     return 0;
 }
