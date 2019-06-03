4913a336e50c7a0dd704c7bbb171800e8ee0fe22
ismdeep <ismdeep@live.com>
2019-06-03 15:47:46 +0800

    Fix up logistic problems

--- python-practice/game-board/main.py
+++ python-practice/game-board/main.py
@@ -59,56 +59,85 @@ def generate_next_nodes(game_board):
 
 
 def search(game_board):
+    # 判断程序是否已经结束
     if end_game_test(game_board):
         return game_equal, None
+    # 根据当前节点，产生接下来的所有可能操作
     next_nodes = generate_next_nodes(game_board)
+    # 判断产生的节点中是否有已经赢的，如果有则直接返回
     for _id_, node in next_nodes:
         if win_test(node):
             return game_win, _id_
     rival_nodes = []
     for _id_, node in next_nodes:
+        # 判断是否已经满了，如果满了则随便返回第一个
         if full_test(node):
             return game_equal, _id_
         flag, new_id = search(inverse(node))
-        rival_nodes.append((flag, new_id))
-    for flag, _id_ in rival_nodes:
+        rival_nodes.append((flag, _id_, new_id))
+    for flag, _id_, _new_id_ in rival_nodes:
         if flag == game_lose:
             return game_win, _id_
-    for flag, _id_ in rival_nodes:
+    for flag, _id_, _new_id_ in rival_nodes:
         if flag == game_equal:
             return game_equal, _id_
-    flag, _id_ = rival_nodes[0]
+    flag, _id_, _new_id_ = rival_nodes[0]
     return game_lose, _id_
+
+
+def symbol_chess(_val_):
+    if _val_ is 1:
+        return 'o'
+    if _val_ is -1:
+        return 'x'
+    return '□'
 
 
 def game_board_to_string(_game_board_):
     ans = ''
-    ans += '%d %d %d\n%d %d %d\n%d %d %d\n' % (
-        _game_board_[0], _game_board_[1], _game_board_[2], _game_board_[3], _game_board_[4], _game_board_[5],
-        _game_board_[6], _game_board_[7], _game_board_[8])
+    ans += '%s %s %s\n%s %s %s\n%s %s %s\n' % (
+        symbol_chess(_game_board_[0]),
+        symbol_chess(_game_board_[1]),
+        symbol_chess(_game_board_[2]),
+        symbol_chess(_game_board_[3]),
+        symbol_chess(_game_board_[4]),
+        symbol_chess(_game_board_[5]),
+        symbol_chess(_game_board_[6]),
+        symbol_chess(_game_board_[7]),
+        symbol_chess(_game_board_[8]))
     return ans
 
 
 def main_demo():
-    result, _id_ = search([1, 0, -1, -1, 1, 0, 1, 1, -1])
+    result, _id_ = search([1, 1, 0, 0, -1, 0, 0, 0, 0])
     print(result, _id_)
 
 
 def main_human_first():
     _game_board_ = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-    print(game_board_to_string(_game_board_) + '\n')
+    print(game_board_to_string(_game_board_))
     while True:
         _human_id_ = int(input())
         _game_board_[_human_id_] = 1
-        print(game_board_to_string(_game_board_) + '\n')
+        print(game_board_to_string(_game_board_))
         result, _robot_id_ = search(inverse(_game_board_))
-        print(result, _robot_id_)
+        print('robot >', result, _robot_id_)
         _game_board_[_robot_id_] = -1
-        print(game_board_to_string(_game_board_) + '\n')
+        print(game_board_to_string(_game_board_))
 
 
 def main_robot_first():
-    pass
+    _game_board_ = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    print(game_board_to_string(_game_board_))
+    while True:
+        result, _robot_id_ = search(inverse(_game_board_))
+        print('robot >', result, _robot_id_)
+        _game_board_[_robot_id_] = -1
+        print(game_board_to_string(_game_board_))
+        _human_id_ = int(input())
+        _game_board_[_human_id_] = 1
+        print(game_board_to_string(_game_board_))
 
 
 main_human_first()
+# main_robot_first()
