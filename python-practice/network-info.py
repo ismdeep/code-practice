54f8fe31872d16f073e9581f7a741e0a0552e19e
ismdeep <ismdeep@live.com>
2019-06-04 15:15:25 +0800

    update

--- python-practice/network-info.py
+++ python-practice/network-info.py
@@ -2,40 +2,29 @@
 import os
 import socket
 import uuid
-
-# 获取主机名
-hostname = socket.gethostname()
-#获取IP
-ip = socket.gethostbyname(hostname)
-# 获取Mac地址
-def get_mac_address():
-    mac=uuid.UUID(int = uuid.getnode()).hex[-12:]
-    return ":".join([mac[e:e+2] for e in range(0,11,2)])
-
-# ipList = socket.gethostbyname_ex(hostname)
-# print(ipList)
-info = {
-    'hostname': hostname,
-    'ip': ip,
-    'mac': get_mac_address()
-}
+import requests
+from ismdeep_utils import ArgvUtil
 
 
-def get_ip_info_from_windows():
-    os.system('mode con cp select=437')
-    content = os.popen('ipconfig /all').read()
-    print(content)
+def get_ip_info():
+    info = {
+        'hostname': socket.gethostname(),
+        'content': os.popen('LANGUAGE=UTF-8.en_US ifconfig').read()
+    }
+    return info
 
 
-def get_ip_info_from_linux():
-    content = os.popen('ifconfig').read()
-    print(content)
+def push_data(_data_):
+    data = {
+        'keyid': 'ip_info_97834',
+        'data': _data_,
+        'token': ArgvUtil.get_sys_argv('-token')
+    }
+    req = requests.post(
+        url='https://info.ismdeep.com/api/info/push_data',
+        data=data
+    )
+    print(req.text)
 
 
-
-# get_ip_info_from_windows()
-get_ip_info_from_linux()
-exit(0)
-
-
-print(info)
+push_data(str(get_ip_info()))
