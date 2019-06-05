87b496ca9653aea6e52c735510a3f92f7628da57
ismdeep <ismdeep@live.com>
2019-06-05 22:48:38 +0800

    Fix network-info.py

--- python-practice/network-info.py
+++ python-practice/network-info.py
@@ -4,19 +4,34 @@ import socket
 import uuid
 import requests
 from ismdeep_utils import ArgvUtil
+from ismdeep_utils import DateTimeUtil
+import logging
+
+logging.basicConfig(
+    filename='run.log',
+    level=logging.DEBUG,
+    format='[%(asctime)s][%(filename)s][line:%(lineno)d] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+def get_mac_address():
+    mac = uuid.UUID(int=uuid.getnode()).hex[-12:]
+    return ":".join([mac[e:e + 2] for e in range(0, 11, 2)])
 
 
 def get_ip_info():
     info = {
         'hostname': socket.gethostname(),
+        'create_datetime': DateTimeUtil.simple_date_time_string(),
         'content': os.popen('LANGUAGE=UTF-8.en_US ifconfig').read()
     }
     return info
 
 
-def push_data(_data_):
+def push_data(_key_id_, _data_):
     data = {
-        'keyid': 'ip_info_97834',
+        'keyid': _key_id_,
         'data': _data_,
         'token': ArgvUtil.get_sys_argv('-token')
     }
@@ -24,7 +39,8 @@ def push_data(_data_):
         url='https://info.ismdeep.com/api/info/push_data',
         data=data
     )
-    print(req.text)
+    return req.text
 
 
-push_data(str(get_ip_info()))
+res_content = push_data('ip_info_%s' % (get_mac_address()), str(get_ip_info()))
+logging.info(res_content)
