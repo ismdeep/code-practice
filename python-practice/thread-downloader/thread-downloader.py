2da905ae121ab06fe2238fa3687344e2f87d3129
ismdeep <ismdeep@live.com>
2019-06-21 16:48:33 +0800

    Add thread-downloader

---
+++ python-practice/thread-downloader/thread-downloader.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+# author: ismdeep
+# dateime: 2019-06-21 08:46:10
+# filename: thread-downloader.py
+# blog: https://ismdeep.com
+
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+
+def get_dns_a(_dns_):
+    return 
+
+
+def content_length(_url_):
+    print(_url_)
+    client = socket(AF_INET, SOCK_STREAM)
