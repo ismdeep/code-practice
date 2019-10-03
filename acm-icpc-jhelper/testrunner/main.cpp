67eea8650f5231aacf439d193aca82dcdbc47b01
ismdeep <ismdeep@live.com>
2019-10-03 15:53:22 +0800

    Update README.md

--- acm-icpc-jhelper/testrunner/main.cpp
+++ acm-icpc-jhelper/testrunner/main.cpp
@@ -1,4 +1,4 @@
-#include "C:/data/projects/code-practice/acm-icpc-jhelper/tasks/Aizu0010.cpp"
+#include "/Users/ismdeep/Projects/code-practice/acm-icpc-jhelper/tasks/Aizu0008.cpp"
 
 #include <iostream>
 #include <fstream>
@@ -29,7 +29,7 @@ bool check(std::string expected, std::string actual) {
 
 int main() {
 	std::vector<jhelper::Test> tests = {
-		{"1\n0.0 0.0 2.0 0.0 2.0 2.0", "1.000 1.000 1.414", true, true},
+		{"35\n1\n", "4\n4", true, true},
 	};
 	bool allOK = true;
 	int testID = 0;
@@ -48,7 +48,7 @@ int main() {
 			std::stringstream in(test.input);
 			std::ostringstream out;
 			std::clock_t start = std::clock();
-			Aizu0010 solver;
+			Aizu0008 solver;
 			solver.solve(in, out);
 			std::clock_t finish = std::clock();
 			double currentTime = double(finish - start) / CLOCKS_PER_SEC;
