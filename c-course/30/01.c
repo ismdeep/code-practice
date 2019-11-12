0f22a791d4357bc9be3cb825ed60fe07082e8fd1
ismdeep <ismdeep@live.com>
2019-11-12 08:17:13 +0800

    Update

---
+++ c-course/30/01.c
@@ -0,0 +1,20 @@
+
+
+
+#include <stdio.h>
+
+#define MAXN 1000
+#define TIMES(id, size) for(int id = 0; id < (size); id++)
+#define ADD(i,j) (i) + (j)
+#define MUL(i,j) (i) * (j)
+
+int main()
+{
+	int a[MAXN];
+	int n = MUL(2,3+4);
+	for (int i = 0; i < n; i++){
+	}
+	TIMES(i, n){
+	}
+	return 0;
+}
---
+++ c-course/30/02.c
@@ -0,0 +1,13 @@
+
+
+#include <stdio.h>
+#include "max_min.h"
+
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	printf("%d\n", max(a, b));
+	return 0;
+}
+
