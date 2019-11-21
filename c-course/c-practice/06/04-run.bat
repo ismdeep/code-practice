f9eb0cba5213001157ba9376af904812913f0fb4
ismdeep <ismdeep@live.com>
2019-11-21 22:33:11 +0800

    Update

--- c-course/c-practice/06/04-run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cls
-gcc 04-gendata.c -o 04-gendata && 04-gendata > 04-in.txt && type 04-in.txt && echo ------------------------------- && gcc -fexec-charset=GBK 04.c -o 04 && 04 < 04-in.txt
-pause
