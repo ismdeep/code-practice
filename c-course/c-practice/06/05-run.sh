9057b7d3c4bdc315f93015f46aae66bcb5c3e128
ismdeep <ismdeep@live.com>
2019-11-21 19:39:44 +0800

    Update

--- c-course/c-practice/06/05-run.sh
+++ c-course/c-practice/06/05-run.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 gcc 05-gendata.c -o 05-gendata
 ./05-gendata > 05-in.txt
 cat 05-in.txt
 echo -------------------------------
-gcc -fexec-charset=UTF-8 05.c -o 05
+gcc 05.c -o 05
 ./05 < 05-in.txt
 
-read -p ""
