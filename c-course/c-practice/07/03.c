11b3bb7eebd2e0f5f024248f00246f8d09427670
ismdeep <ismdeep@live.com>
2019-11-22 03:24:51 +0000

    Add c-practice/07

---
+++ c-course/c-practice/07/03.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int max_in_arr(int *a, int n)
+{
+	int max_val = a[0];
+	for (int i = 1; i < n; i++)
+	{
+		if (a[i] > max_val)
+		{
+			max_val = a[i];
+		}
+	}
+	return max_val;
+}
+
+int main()
+{
+	int n;
+	int *a;
+	scanf("%d", &n);
+	a = malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	print("最大数字为：%d\n", max_in_arr(a, n));
+	return 0;
+}
+
+
