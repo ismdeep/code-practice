a35c72ee79050a061cb822fdd5ac8b92ee9ca007
ismdeep <ismdeep@live.com>
2019-11-25 11:20:14 +0800

    gen-md.sh

---
+++ c-course/c-practice/09/gen-md.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cat 00.md > README.md
+
+for (( i = 1; i <= 2; i++ )); do
+    id=`printf "%02d" ${i}`
+    cat ${id}.md >> README.md
+    echo \`\`\`c >> README.md
+    cat ${id}.c >> README.md
+    echo >> README.md
+    echo \`\`\` >> README.md
+done
+
+echo >> README.md
+echo >> README.md
