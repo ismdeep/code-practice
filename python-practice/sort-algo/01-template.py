00b36c62ff59f2a7c3002ee3cfdc7561d8264eb3
ismdeep <ismdeep@live.com>
2019-11-26 20:45:29 +0800

    Python sort-algo => select sort, quick sort, bubble sort

--- python-practice/sort-algo/01-template.py
+++ python-practice/sort-algo/01-template.py
@@ -17,12 +17,68 @@ class Point:
 class Sorter(object):
     @staticmethod
     def select_sort(data, cmp_func):
+        """
+        Selecte Sort Algorithm
+        :param data:
+        :param cmp_func:
+        :return:
+        """
         for left in range(len(data) - 1):
             m = left
             for i in range(left + 1, len(data)):
                 if cmp_func(data[i], data[m]):
                     m = i
             data[m], data[left] = data[left], data[m]
+
+    @staticmethod
+    def quick_sort(data, cmp_func):
+        """
+        Quick Sort Algorithm
+        :param data:
+        :param cmp_func:
+        :return:
+        """
+        import queue
+        q = queue.Queue()
+        q.put((0, len(data) - 1))
+        while not q.empty():
+            begin, end = q.get()
+            left = begin
+            right = end - 1
+            if begin >= end:
+                continue
+            mid_value = data[end]
+            while left < right:
+                while cmp_func(data[left], mid_value) and left < right:
+                    left += 1
+                while cmp_func(mid_value, data[right]) and left < right:
+                    right -= 1
+                data[left], data[right] = data[right], data[left]
+            if cmp_func(data[left], data[end]):
+                left += 1
+            data[left], data[end] = data[end], data[left]
+            q.put((begin, left - 1))
+            q.put((left + 1, end))
+
+    @staticmethod
+    def bubble_sort(data, cmp_func):
+        """
+        Bubble Sort Algorithm
+        [0, n - 2]
+        [0, n - 3]
+        [0, 0]
+        :param data:
+        :param cmp_func:
+        :return:
+        """
+        for top in range(len(data) - 2, -1, -1):
+            for i in range(top + 1):
+                if not cmp_func(data[i], data[i + 1]):
+                    data[i], data[i + 1] = data[i + 1], data[i]
+
+
+def cmp(a, b):
+    return a <= b
 
 
 def cmp_point(a: Point, b: Point):
@@ -31,15 +87,45 @@ def cmp_point(a: Point, b: Point):
     return da <= db
 
 
-def main():
-    a = [8, 3, 7, -9, -6, 5, -1, 2]
-    b = [1, 3, 4, 5, 6, 7, 2, 1]
-    l = []
-    for i in range(len(a)):
-        l.append(Point(a[i], b[i]))
-    Sorter.select_sort(l, cmp_point)
-    for item in l:
-        print("(%2d,%2d)" % (item.x, item.y))
+def test_quick_sort():
+    for test_id in range(1000):
+        a = []
+        import random
+        for i in range(100):
+            a.append(random.randint(1, 100))
+        b = []
+        for item in a:
+            b.append(item)
+        Sorter.quick_sort(a, cmp)
+        assert a == sorted(b)
 
 
-main()
+def test_select_sort():
+    for test_id in range(1000):
+        a = []
+        import random
+        for i in range(100):
+            a.append(random.randint(1, 100))
+        b = []
+        for item in a:
+            b.append(item)
+        Sorter.select_sort(a, cmp)
+        assert a == sorted(b)
+
+
+def test_bubble_sort():
+    for test_id in range(1000):
+        a = []
+        import random
+        for i in range(100):
+            a.append(random.randint(1, 100))
+        b = []
+        for item in a:
+            b.append(item)
+        Sorter.bubble_sort(a, cmp)
+        assert a == sorted(b)
+
+
+# test_quick_sort()
+# test_select_sort()
+# test_bubble_sort()
