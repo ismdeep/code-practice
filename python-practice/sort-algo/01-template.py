cbdf9480ab3d55e77d29f558ca02f06b73532a82
ismdeep <ismdeep@live.com>
2019-11-27 12:53:58 +0800

    Python sort-algo => merge sort

--- python-practice/sort-algo/01-template.py
+++ python-practice/sort-algo/01-template.py
@@ -2,23 +2,17 @@
 """
 基于模版方法模式允许用户自定义排序规则
 """
-import math
+import time
 
-
-class Point:
-    x = 0
-    y = 0
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+test_count = 100
+test_size = 1000
 
 
 class Sorter(object):
     @staticmethod
     def select_sort(data, cmp_func):
         """
-        Selecte Sort Algorithm
+        Select Sort Algorithm
         :param data:
         :param cmp_func:
         :return:
@@ -64,9 +58,6 @@ class Sorter(object):
     def bubble_sort(data, cmp_func):
         """
         Bubble Sort Algorithm
-        [0, n - 2]
-        [0, n - 3]
-        [0, 0]
         :param data:
         :param cmp_func:
         :return:
@@ -76,22 +67,65 @@ class Sorter(object):
                 if not cmp_func(data[i], data[i + 1]):
                     data[i], data[i + 1] = data[i + 1], data[i]
 
+    @staticmethod
+    def __merge_sort_divide(data, begin, end, cmp_func):
+        """
+        Merge Sort Divide
+        :param data:
+        :param begin:
+        :param end:
+        :param cmp_func:
+        :return:
+        """
+        mid = (begin + end) // 2
+        """
+        Left : [begin ~ mid]
+        Right: [mid + 1 ~ end]
+        """
+        if mid - begin + 1 >= 2:
+            Sorter.__merge_sort_divide(data, begin, mid, cmp_func)
+        if end - mid >= 2:
+            Sorter.__merge_sort_divide(data, mid + 1, end, cmp_func)
+        Sorter.__merge_sort_merge(data, begin, mid + 1, end, cmp_func)
+
+    @staticmethod
+    def __merge_sort_merge(data, begin, mid, end, cmp_func):
+        """
+        Merge Data: [begin ~ mid - 1] and [mid ~ end]
+        :param data:
+        :param begin:
+        :param mid:
+        :param end:
+        :param cmp_func:
+        :return:
+        """
+        index1 = begin
+        index2 = mid
+        while index1 < mid and index2 <= end:
+            if cmp_func(data[index1], data[index2]):
+                index1 += 1
+            else:
+                data[index1], data[index2] = data[index2], data[index1]
+                index1 += 1
+                i = index2
+                while i < end and not cmp_func(data[i], data[i + 1]):
+                    data[i], data[i + 1] = data[i + 1], data[i]
+                    i += 1
+
+    @staticmethod
+    def merge_sort(data, cmp_func):
+        Sorter.__merge_sort_divide(data, 0, len(data) - 1, cmp_func)
+
 
 def cmp(a, b):
     return a <= b
 
 
-def cmp_point(a: Point, b: Point):
-    da = math.sqrt(a.x * a.x + a.y * a.y)
-    db = math.sqrt(b.x * b.x + b.y + b.y)
-    return da <= db
-
-
 def test_quick_sort():
-    for test_id in range(1000):
+    for test_id in range(test_count):
         a = []
         import random
-        for i in range(100):
+        for i in range(test_size):
             a.append(random.randint(1, 100))
         b = []
         for item in a:
@@ -101,10 +135,10 @@ def test_quick_sort():
 
 
 def test_select_sort():
-    for test_id in range(1000):
+    for test_id in range(test_count):
         a = []
         import random
-        for i in range(100):
+        for i in range(test_size):
             a.append(random.randint(1, 100))
         b = []
         for item in a:
@@ -114,10 +148,10 @@ def test_select_sort():
 
 
 def test_bubble_sort():
-    for test_id in range(1000):
+    for test_id in range(test_count):
         a = []
         import random
-        for i in range(100):
+        for i in range(test_size):
             a.append(random.randint(1, 100))
         b = []
         for item in a:
@@ -126,6 +160,23 @@ def test_bubble_sort():
         assert a == sorted(b)
 
 
+def test_merge_sort():
+    for test_id in range(test_count):
+        a = []
+        import random
+        for i in range(test_size):
+            a.append(random.randint(1, 100))
+        b = []
+        for item in a:
+            b.append(item)
+        Sorter.merge_sort(a, cmp)
+        assert a == sorted(b)
+
+
+time_start = time.time()
 # test_quick_sort()
 # test_select_sort()
 # test_bubble_sort()
+# test_merge_sort()
+time_end = time.time()
+print(time_end - time_start)
