cb2c389cfdb58b1054b32be90f558f72e389cc79
ismdeep <ismdeep@live.com>
2020-03-04 10:09:47 +0800

    Update tsgd.py

--- python-practice/crawlers-practice/tsgd/tsgd.py
+++ python-practice/crawlers-practice/tsgd/tsgd.py
@@ -3,13 +3,16 @@
 # dateime: 2018-08-17 13:42:01
 # filename: tsgd.py
 # blog: https://ismdeep.com
-
+import smtplib
 import time
 import datetime
 import urllib
 import http.cookiejar
 import re
 import os
+from email.header import Header
+from email.mime.text import MIMEText
+
 from ismdeep_utils import StringUtil
 import argparse
 import logging
@@ -17,13 +20,14 @@ import logging.config
 import platform
 import redis
 
-
 ################## Configuration START ##################
-redis_host = '127.0.0.1'
-redis_port = 6379
-redis_rent_id_key = 'notify_rent_ids'
-redis_email_username_key = 'email_username'
-redis_email_password_key = 'email_password'
+REDIS_HOST = '127.0.0.1'
+REDIS_PORT = 6379
+REDIS_RENT_ID_KEY = 'notify_rent_ids'
+REDIS_EMAIL_SERVER_KEY = 'email_server'
+REDIS_EMAIL_PORT_KEY = 'email_port'
+REDIS_EMAIL_USERNAME_KEY = 'email_username'
+REDIS_EMAIL_PASSWORD_KEY = 'email_password'
 
 log_file_path = os.environ['HOME']
 if 'Windows' == platform.system():
@@ -37,43 +41,96 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s %(filename)s[%(levelname)s][line:%(lineno)d] %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S')
+
+
 ################## Configuration END   ##################
-
-
 
 
 def date_need_notify(__to_date__):
     tmp = time.strptime(__to_date__, '%Y/%m/%d')
     obj = datetime.datetime(tmp.tm_year, tmp.tm_mon, tmp.tm_mday)
     time_now = datetime.datetime.now()
-    if (obj - time_now).days <= 7:
+    if (obj - time_now).days <= 70:
         return True
     return False
 
 
+def redis_conn():
+    conn = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
+    return conn
+
+
 # 从 Redis 数据库查询 __rent_id__ 是否存在
 def query_notify_rent_id(__rent_id__):
-    conn = redis.Redis(host=redis_host, port=redis_port)
-    return conn.sismember(redis_rent_id_key, __rent_id__)
+    conn = redis_conn()
+    ans = conn.sismember(REDIS_RENT_ID_KEY, __rent_id__)
+    conn.close()
+    return ans
 
 
 # 往 Redis 数据库写入 __rent_id__
 def push_notify_rent_id(__rent_id__):
-    import redis
-    conn = redis.Redis(host=redis_host, port=redis_port)
-    conn.sadd(redis_rent_id_key, __rent_id__)
+    conn = redis_conn()
+    conn.sadd(REDIS_RENT_ID_KEY, __rent_id__)
+    conn.close()
 
 
 # TODO
 # 发送邮件同志
 def send_email_notify(__book_name__, __from_date__, __to_date__, __fullname__):
-    pass
+    logging.info("Send mail [%s:《%s》] %s ~ %s" % (__fullname__, __book_name__, __from_date__, __to_date__))
+    content = """%s借阅的《%s》快到期了，借阅时间：%s ~ %s.""" % (
+        __fullname__, __book_name__, __from_date__, __to_date__
+    )
+    while not send_email('ismdeep@icloud.com', '书籍逾期提醒', content, 'text'):
+        time.sleep(1)
+
+
+class EmailAccount(object):
+    def __init__(self):
+        self.server = None
+        self.port = None
+        self.email = None
+        self.password = None
+
+
+def load_email_account():
+    conn = redis_conn()
+    email_account = EmailAccount()
+    email_account.server = conn.get(REDIS_EMAIL_SERVER_KEY).decode()
+    email_account.port = int(conn.get(REDIS_EMAIL_PORT_KEY))
+    email_account.email = conn.get(REDIS_EMAIL_USERNAME_KEY).decode()
+    email_account.password = conn.get(REDIS_EMAIL_PASSWORD_KEY).decode()
+    return email_account
+
+
+def send_email(to_email, title, content, content_type):
+    email_account = load_email_account()
+    logging.info('start sending email to %s' % to_email)
+    sender = email_account.email
+    receivers = to_email
+    message = MIMEText(content, content_type, 'utf-8')
+    message['From'] = email_account.email
+    message['To'] = to_email
+    subject = title
+    message['Subject'] = Header(subject, 'utf-8')
+
+    try:
+        smtp_obj = smtplib.SMTP_SSL(email_account.server, email_account.port)
+        smtp_obj.connect(email_account.server, email_account.port)
+        smtp_obj.login(sender, email_account.password)
+        smtp_obj.sendmail(sender, receivers, message.as_string())
+        logging.info('send email to %s successfully' % to_email)
+        return True
+    except smtplib.SMTPException as e:
+        return False
 
 
 class JxustTSG:
     fullname = ''
     cj = http.cookiejar.CookieJar()
-    home_site = 'http://218.87.136.111'
+    # home_site = 'http://218.87.136.111'
+    home_site = 'http://tsgweb.jxust.edu.cn'
     username = ''
     password = ''
     header = {
