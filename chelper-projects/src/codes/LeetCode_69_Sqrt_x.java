b771159c805ed4692892589814c604876e4693eb
ismdeep <ismdeep@protonmail.com>
2020-07-20 09:46:46 +0800

    Add LeetCode_69_Sqrt_x

---
+++ chelper-projects/src/codes/LeetCode_69_Sqrt_x.java
@@ -0,0 +1,16 @@
+package codes;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class LeetCode_69_Sqrt_x {
+
+    class Solution {
+        public int mySqrt(int x) {
+            return (int) Math.sqrt(x);
+        }
+    }
+
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+    }
+}
