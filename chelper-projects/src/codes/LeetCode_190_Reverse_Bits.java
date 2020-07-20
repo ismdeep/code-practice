c0f106b568727f0736be54a9b00bb6dfcd4da292
ismdeep <ismdeep@protonmail.com>
2020-07-20 16:42:19 +0800

    Add LeetCode_190_Reverse_Bits

---
+++ chelper-projects/src/codes/LeetCode_190_Reverse_Bits.java
@@ -0,0 +1,19 @@
+package codes;
+
+import jdk.jfr.Unsigned;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class LeetCode_190_Reverse_Bits {
+    public class Solution {
+        // you need treat n as an unsigned value
+        public int reverseBits(int n) {
+            return Integer.reverse(n);
+        }
+    }
+
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+        System.out.println((new Solution()).reverseBits(43261596));
+    }
+}
